b4f1bbcda8c4575905211c9859faec4ab7dfe0fa
Student <student@example.com>
2026-09-24 19:23:53 +0700

    Tipe Data Dasar

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,10 @@
 fun main() {
     val customers = 10
     println("There are $customers customers")
+
+    // Tipe Data Dasar
+    val d: Int
+    d = 3
+    val e: String = "hello"
+    println("d = $d, e = $e")
 }
